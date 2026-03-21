bd010d6098333db2358bc071acd214dc9b6b09e9
dav1dchaparro <jchaparro@fi.uba.ar>
2026-03-21 10:47:07 -0300

    init andriod studio

--- backend/app/models/__init__.py
+++
@@ -1,5 +0,0 @@
-from app.models.user import User
-from app.models.category import Category
-from app.models.product import Product
-from app.models.sale import Sale
-from app.models.sale_item import SaleItem
